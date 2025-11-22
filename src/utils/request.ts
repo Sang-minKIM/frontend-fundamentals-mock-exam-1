08fe93f738dadb8e8a405066b19e94a083f279b2
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-11-22 12:37:19 +0900

    chore: request를 함수 선언문으로 변경

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,14 +4,14 @@ import { http, isHttpError } from 'tosslib';
 const BASE_URL = 'http://localhost:5173';
 
 // tosslib의 http를 래핑한 request 함수
-export const request = async <T>(
+export async function request<T>(
   path: string,
   config?: {
     method?: 'get' | 'post' | 'delete' | 'patch';
     json?: unknown;
     headers?: Record<string, string>;
   }
-): Promise<T> => {
+): Promise<T> {
   try {
     const url = `${BASE_URL}${path}`;
     const method = config?.method || 'get';
@@ -33,4 +33,4 @@ export const request = async <T>(
       throw new Error('알 수 없는 에러입니다.');
     }
   }
-};
+}
