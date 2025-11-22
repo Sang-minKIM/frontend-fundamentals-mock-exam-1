5555e72e872d6f6f7ad6bedcfe9d04a8e8abf3f4
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2025-11-22 11:36:30 +0900

    feat: suspensive 패키지 추가 및 global suspense, errorboundary 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
+
 import { QueryClientProvider } from './providers/QueryClientProvider';
+
+import { ErrorBoundary, Suspense } from '@suspensive/react';
 
 export function App() {
   return (
     <>
       <GlobalStyles />
       <GlobalPortal.Provider>
-        <QueryClientProvider>
-          <Routes />
-        </QueryClientProvider>
+        <ErrorBoundary fallback={<div>Error</div>}>
+          <QueryClientProvider>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes />
+            </Suspense>
+          </QueryClientProvider>
+        </ErrorBoundary>
       </GlobalPortal.Provider>
     </>
   );
